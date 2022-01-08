c160f2c1c7d0786ef8988b24e49e837a9a339ea0
sullivan986 <2257501878@qq.com>
2022-01-08 14:59:34 +0000

    d2

--- problems/002_add_two_numbers/main.cpp
+++ problems/002_add_two_numbers/main.cpp
@@ -1,63 +1,15 @@
-#include <iostream>
-#include <map>
-#include <unordered_map>
-#include <vector>
 
-#include "benchmark/benchmark.h"
 
-using namespace std;
-
-class Solution
+struct ListNode
 {
-public:
-    static vector<int> twoSum_unordered_map(vector<int> &nums, int target)
-    {
-        unordered_map<int, int> hashtable;
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            auto it = hashtable.contains(target - nums[i]);
-            if (it)
-            {
-                return {hashtable[target - nums[i]], i};
-            }
-            hashtable[nums[i]] = i;
-        }
-        return {};
-    }
-
-    static vector<int> twoSum_enumeration(vector<int> &nums, int target)
-    {
-        for (int16_t i = 0; i < nums.size(); i++)
-        {
-            for (int j = 0; j < nums.size(); j++)
-            {
-                if (nums[i] + nums[j] == target)
-                {
-                    return {i, j};
-                }
-            }
-        }
-    }
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-static void bench_hashtable(benchmark::State &state)
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 {
-    vector<int> e{2, 3, 4, 5, 6};
-    auto c = Solution::twoSum_unordered_map(e, 9);
-    for (size_t i = 0; i < c.size(); i++)
-    {
-        cout << e[c[i]] << endl;
-    }
+    ListNode *head = nullptr;
 }
-BENCHMARK(bench_hashtable);
-
-static void bench_enumeration(benchmark::State &state)
-{
-    vector<int> e{2, 3, 4, 5, 6};
-    auto c = Solution::twoSum_enumeration(e, 9);
-    for (size_t i = 0; i < c.size(); i++)
-    {
-        cout << e[c[i]] << endl;
-    }
-}
-BENCHMARK(bench_enumeration)
