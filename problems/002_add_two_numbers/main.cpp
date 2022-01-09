a2343d9ce29f21e3ea061910796567c6e18e09ba
sullivan986 <2257501878@qq.com>
2022-01-09 15:23:14 +0000

    d2

--- problems/002_add_two_numbers/main.cpp
+++ problems/002_add_two_numbers/main.cpp
@@ -1,4 +1,4 @@
-
+#include "benchmark/benchmark.h"
 
 struct ListNode
 {
@@ -12,4 +12,40 @@ struct ListNode
 ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 {
     ListNode *head = nullptr;
+    ListNode *tail = nullptr;
+    int carry = 0;
+    while (l1 || l2)
+    {
+        int n1 = l1 ? l1->val : 0;
+        int n2 = l2 ? l2->val : 0;
+        int sum = n1 + n2 + carry;
+        if (!head)
+        {
+            head = tail = new ListNode(sum % 10);
+        }
+        else
+        {
+            tail->next = new ListNode(sum % 10);
+            tail = tail->next;
+        }
+        carry = sum / 10;
+        if (l1)
+        {
+            l1 = l1->next;
+        }
+        if (l2)
+        {
+            l2 = l2->next;
+        }
+    }
+    if (carry > 0)
+    {
+        tail->next = new ListNode(carry);
+    }
+    return head;
 }
+
+static void addtwonums_m(benchmark::State &state)
+{
+}
+BENCHMARK(addtwonums_m);
