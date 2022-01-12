ff5332d30df01827d4e847d9d7455e44a21bc435
sullivan986 <2257501878@qq.com>
2022-01-12 13:18:47 +0000

    d3

--- problems/003_longest_substring_without_repeating_characters/main.cpp
+++ problems/003_longest_substring_without_repeating_characters/main.cpp
@@ -1,5 +1,8 @@
 #include <string>
 #include <iostream>
+#include <unordered_set>
+
+const std::string str("a");
 
 // 第一反应有限状态机？
 // -----------------------------------------------------------------
@@ -11,8 +14,46 @@
 // -----------------------------------------------------------------
 // 开头数               位数归零                    length = 0
 
+// 算了，暴力枚举法
 int lengthOfLongestSubstring(std::string s)
 {
-    auto t = s.begin();
-    auto sc = s.size();
+    if (str.size() == 0)
+    {
+        return 0;
+    }
+
+    auto it = s.begin();
+    std::string::iterator b;
+    int u, u1 = 1;
+    for (size_t i = 0; i < s.size(); i++)
+    {
+        u = 1;
+        b = it + 1;
+        while (*b != *it && it != s.end() - 1)
+        {
+
+            if (b == s.end())
+            {
+                u = 0;
+                break;
+            }
+
+            b++;
+            u++;
+        }
+        if (u > u1)
+        {
+            u1 = u;
+        }
+
+        it++;
+    }
+    return u1;
+}
+
+int main(int argc, char const *argv[])
+{
+    int u = lengthOfLongestSubstring(str);
+    std::cout << u;
+    return 0;
 }
