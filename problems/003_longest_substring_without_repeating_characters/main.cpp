052494d7412e051a72f07910ac51e9b2eaf1b232
sullivan986 <2257501878@qq.com>
2022-01-13 15:28:33 +0000

    d3

--- problems/003_longest_substring_without_repeating_characters/main.cpp
+++ problems/003_longest_substring_without_repeating_characters/main.cpp
@@ -13,47 +13,52 @@ const std::string str("a");
 // 不同的字符           位数加一                    提取位数并且下一个为开头数
 // -----------------------------------------------------------------
 // 开头数               位数归零                    length = 0
-
 // 算了，暴力枚举法
+// 错误示范
+// int lengthOfLongestSubstring(std::string s)
+// {
+//     if (str.size() == 0)
+//     {
+//         return 0;
+//     }
+//
+//     auto it = s.begin();
+//     std::string::iterator b;
+//     int u, u1 = 1;
+//     for (size_t i = 0; i < s.size(); i++)
+//     {
+//         u = 1;
+//         b = it + 1;
+//         while (*b != *it && it != s.end() - 1)
+//         {
+//
+//             if (b == s.end())
+//             {
+//                 u = 0;
+//                 break;
+//             }
+//
+//             b++;
+//             u++;
+//         }
+//         if (u > u1)
+//         {
+//             u1 = u;
+//         }
+//
+//         it++;
+//     }
+//     return u1;
+// }
+
 int lengthOfLongestSubstring(std::string s)
 {
-    if (str.size() == 0)
-    {
-        return 0;
-    }
-
-    auto it = s.begin();
-    std::string::iterator b;
-    int u, u1 = 1;
-    for (size_t i = 0; i < s.size(); i++)
-    {
-        u = 1;
-        b = it + 1;
-        while (*b != *it && it != s.end() - 1)
-        {
-
-            if (b == s.end())
-            {
-                u = 0;
-                break;
-            }
-
-            b++;
-            u++;
-        }
-        if (u > u1)
-        {
-            u1 = u;
-        }
-
-        it++;
-    }
-    return u1;
+    std::unordered_set<char> occ;
 }
 
-int main(int argc, char const *argv[])
-{
-    int u = lengthOfLongestSubstring(str);
-    std::cout << u;
-    return 0;
-}
+// int main(int argc, char const *argv[])
+// {
+//     int u = lengthOfLongestSubstring(str);
+//     std::cout << u;
+//     return 0;
+// }
---
+++ problems/004_median_of_two_sorted_arrays/main.cpp
@@ -0,0 +1,5 @@
+#include <vector>
+
+double findMedianSortedArrays(std::vector<int> &nums1, std::vector<int> &nums2)
+{
+}
