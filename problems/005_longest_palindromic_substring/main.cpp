e97d7197240c6cb3771db1c22b7efc9c30f7686a
sullivan986 <2257501878@qq.com>
2022-01-17 18:27:03 +0000

    sleep

--- problems/005_longest_palindromic_substring/main.cpp
+++ problems/005_longest_palindromic_substring/main.cpp
@@ -2,31 +2,49 @@
 #include <vector>
 #include <iostream>
 
+// 瞎写
 std::string longestPalindrome(std::string s)
 {
-    std::string rt = *s.begin();
-    std::string rtt = *s.begin();
-    int32_t sz = s.size();
-    auto it1 = s.begin() + 1;
+    std::string rt;
+    std::string rtt{*s.begin()};
+    std::string temp;
+    auto it1 = s.begin();
     auto it2 = it1;
 
-    while (it1 != it2)
+    while (it1 != s.end() - 1)
     {
         while (it2 != s.end())
         {
             rt.push_back(*it2);
-            if (rt = std::reverse(rt.begin(), rt.end()))
+            for (int32_t i = rt.size() - 1; i >= 0; i--)
             {
-                (int32_t) rt.size() >
+                temp.push_back(rt[i]);
+
+                if (rt == temp && rt.size() > rtt.size())
+                {
+                    rtt = rt;
+                }
             }
+            temp.clear();
+
+            it2++;
         }
+        rt.clear();
+        it1++;
+        it2 = it1;
     }
     return rtt;
 }
 
+// 动态规划解法
+std::string longestPalindrome2(std::string s)
+{
+}
+
+std::string y("babad");
+
 int main(int argc, char const *argv[])
 {
-    std::string u{"123321"};
-    std::cout << *u.end();
+    std::cout << longestPalindrome(y);
     return 0;
 }
