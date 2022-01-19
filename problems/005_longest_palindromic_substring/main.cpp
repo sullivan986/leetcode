360412a0a8b98edcebd187ae2dc870c184ee4f43
sullivan986 <2257501878@qq.com>
2022-01-19 14:27:41 +0000

    temp

--- problems/005_longest_palindromic_substring/main.cpp
+++ problems/005_longest_palindromic_substring/main.cpp
@@ -70,15 +70,104 @@ std::string longestPalindrome1(std::string s)
     return rt;
 }
 
-// 动态规划
-std::string longestPalindrome1(std::string s)
+// 官方的中心法好一些
+std::pair<int, int> expandAroundCenter(const std::string &s, int left, int right)
 {
+    while (left >= 0 && right < s.size() && s[left] == s[right])
+    {
+        --left;
+        ++right;
+    }
+    return {left + 1, right - 1};
 }
 
-std::string y("cbbc");
+std::string longestPalindrome2(std::string s)
+{
+    int start = 0, end = 0;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        auto [left1, right1] = expandAroundCenter(s, i, i);
+        auto [left2, right2] = expandAroundCenter(s, i, i + 1);
+        if (right1 - left1 > end - start)
+        {
+            start = left1;
+            end = right1;
+        }
+        if (right2 - left2 > end - start)
+        {
+            start = left2;
+            end = right2;
+        }
+    }
+    return s.substr(start, end - start + 1);
+}
+
+// 官方的动态规划,但是又没有完全的动态规划
+
+std::string longestPalindrome3(std::string s)
+{
+
+    int n = s.size();
+    if (n < 2)
+    {
+        return s;
+    }
+
+    int maxLen = 1;
+    int begin = 0;
+    // dp[i][j] 表示 s[i..j] 是否是回文串
+    std::vector<std::vector<int>> dp(n, std::vector<int>(n));
+    // 初始化：所有长度为 1 的子串都是回文串
+    for (int i = 0; i < n; i++)
+    {
+        dp[i][i] = true;
+    }
+    // 递推开始
+    // 先枚举子串长度
+    for (int L = 2; L <= n; L++)
+    {
+        // 枚举左边界，左边界的上限设置可以宽松一些
+        for (int i = 0; i < n; i++)
+        {
+            // 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+            int j = L + i - 1;
+            // 如果右边界越界，就可以退出当前循环
+            if (j >= n)
+            {
+                break;
+            }
+
+            if (s[i] != s[j])
+            {
+                dp[i][j] = false;
+            }
+            else
+            {
+                if (j - i < 3)
+                {
+                    dp[i][j] = true;
+                }
+                else
+                {
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+            }
+
+            // 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+            if (dp[i][j] && j - i + 1 > maxLen)
+            {
+                maxLen = j - i + 1;
+                begin = i;
+            }
+        }
+    }
+    return s.substr(begin, maxLen);
+}
+
+std::string y("babad");
 
 int main(int argc, char const *argv[])
 {
-    std::cout << longestPalindrome1(y);
+    std::cout << longestPalindrome3(y);
     return 0;
 }
