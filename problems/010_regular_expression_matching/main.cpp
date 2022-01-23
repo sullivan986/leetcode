09f6a399207700278ffc94e7ae4abd6e276dd768
sullivan986 <2257501878@qq.com>
2022-01-23 15:47:39 +0000

    d7

---
+++ problems/010_regular_expression_matching/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+bool isMatch(std::string s, std::string p)
+{
+    for (auto str : p)
+    {
+        /* code */
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
