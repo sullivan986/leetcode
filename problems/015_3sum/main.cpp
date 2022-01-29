325f57b42006d403ba3ceb30221e4ffb491b13f2
sullivan986 <2257501878@qq.com>
2022-01-30 00:55:56 +0800

    qaq

--- problems/015_3sum/main.cpp
+++ problems/015_3sum/main.cpp
@@ -1,6 +1,49 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+std::vector<std::vector<int>> threeSum1(std::vector<int> &nums)
+{
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    std::vector<std::vector<int>> ans;
+    // 枚举 a
+    for (int first = 0; first < n; ++first)
+    {
+        // 需要和上一次枚举的数不相同
+        if (first > 0 && nums[first] == nums[first - 1])
+        {
+            continue;
+        }
+        // c 对应的指针初始指向数组的最右端
+        int third = n - 1;
+        int target = -nums[first];
+        // 枚举 b
+        for (int second = first + 1; second < n; ++second)
+        {
+            // 需要和上一次枚举的数不相同
+            if (second > first + 1 && nums[second] == nums[second - 1])
+            {
+                continue;
+            }
+            // 需要保证 b 的指针在 c 的指针的左侧
+            while (second < third && nums[second] + nums[third] > target)
+            {
+                --third;
+            }
+            // 如果指针重合，随着 b 后续的增加
+            // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+            if (second == third)
+            {
+                break;
+            }
+            if (nums[second] + nums[third] == target)
+            {
+                ans.push_back({nums[first], nums[second], nums[third]});
+            }
+        }
+    }
+    return ans;
+}
 
 std::vector<std::vector<int>> threeSum(std::vector<int> &nums)
 {
@@ -11,20 +54,41 @@ std::vector<std::vector<int>> threeSum(std::vector<int> &nums)
     }
     std::vector<std::vector<int>> ret;
     std::sort(std::begin(nums), std::end(nums));
-    std::vector<int> temp = {nums[0], nums[1], nums[2]};
+
+    if ((int)nums.size() == 3 && nums[0] == nums[1] && nums[1] == nums[2] && nums[2] == 0)
+    {
+        return {{0, 0, 0}};
+    }
 
     int l;
     int r;
 
     int temp1;
 
+    int nums1, nums2, nums3;
+
     for (int32_t i = 0; i < sz - 2; ++i)
     {
-        l = 1;
+        l = i + 1;
         r = sz - 1;
         while (l < r)
         {
-            temp1 = nums[i] + nums[l] + nums[r];
+            nums1 = nums[i];
+            nums2 = nums[l];
+            nums3 = nums[r];
+            temp1 = nums1 + nums2 + nums3;
+
+            if (nums2 == nums[l + 1] && l + 1 < r)
+            {
+                ++l;
+                continue;
+            }
+            if (nums3 == nums[r - 1] && r - 1 > l)
+            {
+                --r;
+                continue;
+            }
+
             if (temp1 < 0)
             {
                 ++l;
@@ -35,7 +99,14 @@ std::vector<std::vector<int>> threeSum(std::vector<int> &nums)
             }
             else
             {
-                ret.push_back({nums[i], nums[l++], nums[r--]});
+                if (nums[i] == nums[i + 1] && l != i + 1)
+                {
+                    break;
+                }
+                else
+                {
+                    ret.push_back({nums[i], nums[l++], nums[r--]});
+                }
             }
         }
     }
@@ -43,7 +114,7 @@ std::vector<std::vector<int>> threeSum(std::vector<int> &nums)
     return ret;
 }
 
-std::vector<int> a = {-1, 0, 1, 2, -1, -4};
+std::vector<int> a = {1, 1, -2};
 
 int main(int argc, char const *argv[])
 {
@@ -54,7 +125,7 @@ int main(int argc, char const *argv[])
         {
             std::cout << u[i][j] << " ";
         }
+        std::cout << "\n";
     }
-
     return 0;
 }
